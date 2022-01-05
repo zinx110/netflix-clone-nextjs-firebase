69b9467c4d30c45f07d5172ee963c751922b0389
zinx110 <tasdid110@gmail.com>
2022-01-05 14:11:07 +0600

    error fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import styles from "./Layout.module.css";
@@ -5,6 +6,9 @@ import styles from "./Layout.module.css";
 export default function Layout({ children }) {
   return (
     <div className={styles.app}>
+      <Head>
+        <title>Netflix & Chill</title>
+      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
